c08fb27f62830f77c79dba4f6f0c0eb67a971cc4
unknown <predragsatelit@gmail.com>
2024-02-07 20:10:27 +0100

    domaci

--- e2e/creatGallery.cy.js
+++ e2e/creatGallery.cy.js
@@ -31,3 +31,24 @@ it("Succsessfull creation of gallery", ()=>{
     cy.url().should("not.contain" , "/create");
     cy.get("h1").should('contain', "All Galleries");
 });
+
+
+it.only("Succsessfull creation of gallery", ()=>{
+    //Login
+    //Creation of new gallery
+    cy.get(locators.craeteGallery.pageURL).click();
+    cy.url().should("contain" , "/create");
+    cy.get(locators.commonElements.heading).should("contain" , "Create Gallery");
+    cy.get(locators.commonElements.label).eq(0).should("have.text" , "Title:");
+    cy.get(locators.commonElements.label).eq(1).should("have.text" , "Descriptions:");
+    cy.get(locators.commonElements.label).eq(2).should("contain" , "Images:");
+    createGallery.createGalleryFunction("Test gallery", "dasdadadad", "https://cdn.pixabay.com/photo/2018/07/31/22/08/lion-3576045_1280.jpg");
+    //Redirecting to All Galleries page
+    cy.url().should("not.contain" , "/create");
+    cy.get(locators.commonElements.heading).should('contain', "All Galleries");
+    cy.get(".box-title").eq(0).click();
+    cy.get("button").contains("Delete Gallery").click();
+    cy.window().then((win) => {
+        cy.stub(win, 'confirm').returns(true); // Simuliraj pritisak na "OK"
+      });
+});
